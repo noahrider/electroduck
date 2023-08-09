0462117103bd7c121034556b50fab79e6e37efc2
Noah Rider <97214639+noahrider@users.noreply.github.com>
2023-08-09 06:45:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const commandInput = document.getElementById("commandInput");
-    const output = document.getElementById("output");
-    const prompt = document.getElementById("prompt");
+// Used to hold existing open modal
+openModal = undefined;
 
-    // Hide the prompt initially
-    prompt.style.display = "none";
+function modalBackgroundClick(event, modalName)
+{
+  var modal = document.getElementById(modalName);
+  if (event.target === modal) {
+    turnOffModal(modal);
+  }
+}
 
-    // Startup message
-    output.innerHTML += "Starting DOM-DOS<br>";
-    setTimeout(function() {
-        output.innerHTML += "<i>Type in help for more.</i>";
-        setTimeout(function() {
-        output.innerHTML += "<br><br>DOM-DOS is testing extended memory... ";
-        setTimeout(function() {
-            output.innerHTML += "done.<br>";
-            setTimeout(function() {
-                prompt.style.display = "inline"; // Show the prompt after a delay
-            }, 500);
-        }, 2000);
-    }, 3000);
-}, 1000);
+function turnOnModal(modalName, event) {
+  if (modal == undefined) {
+    var modal = document.getElementById(modalName);
+    modal.classList.add('bg-active');
+    openModal = modal;
+  }
+}
 
-
-    commandInput.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            handleCommand(commandInput.value.trim());
-            commandInput.value = "";
-        }
-    });
-
-    function handleCommand(command) {
-        output.innerHTML += `<span class="command-line">C:>${command}</span><br>`;
-        if (command === "read apple-collection.txt") {
-            output.innerHTML += "APPLE COLLECTION:<br>Nothing here... yet.";
-        } else if (command === "read cameras.txt") {
-            output.innerHTML += "CAMERA COLLECTION:<br>Canon PowerShot [WORKING]<br>Nixon CoolPix [WORKING]";
-        } else if (command === "read contact.txt") {
-            output.innerHTML += "CONTACT:<br>dominic@electroduck.net (me)<br>dev@noahrider.com (webmaster)";
-        } else if (command === "READ apple-collection.txt") {
-                output.innerHTML += "APPLE COLLECTION:<br>Nothing here... yet.";
-            } else if (command === "READ cameras.txt") {
-                output.innerHTML += "CAMERA COLLECTION:<br>Canon PowerShot [WORKING]<br>Nixon CoolPix [WORKING]";
-            } else if (command === "READ contact.txt") {
-                output.innerHTML += "CONTACT:<br>dominic@electroduck.net (me)<br>dev@noahrider.com (webmaster)";
-        } else if (command === "help") {
-            output.innerHTML += "Commands:<br>READ: read a file (eg: read contact.txt <br>Files: apple-collection.txt, cameras.txt, contact.txt";
-        } else if (command === "credits") {
-            output.innerHTML += "Website created by Noah Rider [dev@noahrider.com]";
-        } else {
-            output.innerHTML += "Command not recognized.<br>";
-        }
-    }
-});
+function turnOffModal(modalName) {
+  var modal = document.getElementById(modalName);
+  modal.classList.remove('bg-active');
+  openModal = null;
+}
